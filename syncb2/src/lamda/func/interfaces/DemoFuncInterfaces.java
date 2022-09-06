449045ebb2ea3fa988288b9348d404eeb17a4a4a
abdul ansari <abdul.tanveer@gmail.com>
2022-09-06 17:03:37 +0530

    consumer and supplier intefaces

--- syncb2/src/lamda/func/interfaces/DemoFuncInterfaces.java
+++ syncb2/src/lamda/func/interfaces/DemoFuncInterfaces.java
@@ -1,6 +1,9 @@
 package lamda.func.interfaces;
 
+import java.util.function.Consumer;
+import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 
 public class DemoFuncInterfaces {
 	
@@ -18,6 +21,15 @@ public class DemoFuncInterfaces {
 		
 	System.out.println(isEven.test(3));
 	System.out.println(isOdd.test(4));
+	
+	Function<String, Integer> strLen = (str) -> {return str.length();};
+	System.out.println(strLen.apply("synchronoss"));
+	
+	Consumer<String> greet = (str)->{System.out.println("welcome "+str);};
+	greet.accept("ansar");
+	
+	Supplier<Integer> otpSupplier = ()-> {return 98765;};
+	System.out.println("otp is"+ otpSupplier.get());
 	}
 
 }
